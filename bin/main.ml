fccb7cd8b97c30e6accdc193b08843f6d9bcbb01
Nikita Gaidakov <ngaidakov@podfather.com>
2025-11-27 21:06:00 +0000

    Transform vim input to zed json

--- bin/main.ml
+++ bin/main.ml
@@ -3,15 +3,11 @@ open Vim2zed
 let () =
   match Array.to_list Sys.argv with
   | [_; filename] ->
-      let mappings = Vim.parse_file filename in
-      Vim.pretty_print mappings;
-
-      print_endline "";
-      print_endline "----- Default keymap -----";
-      print_endline "";
-
-      let keymap = Zed.Keymap.from_file "data/default-keymap.json" in
-      print_endline @@ Zed.Print.keymap keymap
+      (* let input_keymap = Zed.Keymap.from_file "data/default-keymap.json" in *)
+      let vim_mappings = Vim.parse_file filename in
+      let zed_keymap = To.keymap_of_vim vim_mappings in
+      let output_json = Zed.Keymap.to_json zed_keymap in
+      print_endline @@ Yojson.Safe.pretty_to_string output_json
   | _ ->
       Printf.eprintf "Usage: %s <vim_file>\n" Sys.argv.(0);
       exit 1
